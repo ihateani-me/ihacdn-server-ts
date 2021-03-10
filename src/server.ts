72bc4597eab2727b978ab748138669a56c23b293
noaione <noaione0809@gmail.com>
2021-03-10 20:36:25 +0700

    Use geoip for analytics

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,18 @@ import cron from "node-cron";
 import express from "express";
 import * as cons from "consolidate";
 import express_compression from "compression";
-import winston from "winston";
-import { logger as WinstonLog, errorLogger as WinstonErrorLog } from "express-winston";
+import express_cors from "cors";
 import { extname, join } from "path";
 import { existsSync, readFile } from "fs";
 
-import config from "./config.json";
+import config from "./config";
 import { humanizebytes, is_none } from "./utils/swissknife";
 import { AdminRoute, ShortenerAPI, UploadAPI } from "./routes";
 import { RedisDB } from "./utils/redisdb";
 import { DELETED_ERROR } from "./utils/error_message";
 import { clearExpiredFile } from "./utils/file_retention";
+import { expressErrorLogger, expressLogger, logger as MainLogger } from "./utils/logger";
+import { getCountry, init as GeoIPInit } from "./utils/geoip";
 
 // @ts-ignore
 const REDIS_INSTANCE = new RedisDB(config.redisdb.host, config.redisdb.port, config.redisdb.password);
@@ -24,51 +25,29 @@ app.engine("html", cons.atpl);
 app.set("view engine", "html");
 app.set("views", __dirname + "/views");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Content-type,Accept,x-access-token,X-Key"
-    );
-    if (req.method == "OPTIONS") {
-        res.status(200).end();
-    } else {
-        next();
-    }
-});
+// @ts-ignore
+app.use(express_cors());
 
-app.use(WinstonLog({
-    transports: [
-        new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.ms(),
-        winston.format.printf((info) => {
-            return `[${info["timestamp"]}][${info.level}]: HTTP ${info["meta"]["req"]["httpVersion"]} ${info.message}`;
-        })
-    ),
-    expressFormat: true,
-    colorize: true,
-}))
-
+app.use(expressLogger);
 app.use(express_compression());
 app.use("/static", express.static(join(__dirname, "assets")));
 
 app.get("/", (_, res) => {
+    let fslimit = "None";
+    if (typeof config.storage.filesize_limit === "number") {
+        fslimit = humanizebytes(config.storage.filesize_limit * 1024)
+    }
     res.render("home", {
         hostname: config.hostname,
         https_mode: config.https_mode,
-        FSIZE_LIMIT: humanizebytes(config.storage.filesize_limit * 1024),
+        FSIZE_LIMIT: fslimit,
         BLACKLIST_EXTENSION: config.blocklist.extension,
         BLACKLIST_CTYPES: config.blocklist.content_type,
         ENABLE_FILE_RETENTION: config.file_retention.enable,
         FILE_RETENTION_MIN_AGE: config.file_retention.min_age,
         FILE_RETENTION_MAX_AGE: config.file_retention.max_age
     })
-})
+});
 
 app.get("/robots.txt", (_, res) => {
     res.send(
@@ -94,7 +73,44 @@ app.use("/upload", UploadAPI);
 app.use("/short", ShortenerAPI);
 app.use("/admin", AdminRoute);
 
-app.get("/:idpath", (req, res) => {
+async function logAnalytics(key: string, country: string) {
+    await REDIS_INSTANCE.client.incr(`iharedir_stats_${key}_hits`);
+    await REDIS_INSTANCE.client.lpush(`iharedir_stats_${key}_country`, country);
+}
+
+function realGetCountry(ipSets: string[]): string {
+    let cc: string;
+    let localDetect = false;
+    for (let index = 0; index < ipSets.length; index++) {
+        let test = getCountry(ipSets[index]);
+        if (test === "Local") {
+            localDetect = true;
+        }
+        // @ts-ignore
+        if (!["Unknown", "Local"].includes(test) && typeof cc !== "string") {
+            cc = test;
+            break;
+        }
+    }
+    // @ts-ignore
+    if (typeof cc === "string") {
+        return cc;
+    }
+    return localDetect ? "Local" : "Unknown";
+}
+
+app.get("/:idpath", async (req, res) => {
+    const logger = MainLogger.child({fn: "CDNMapping"});
+    let ipaddr = req.headers["x-forwareded-for"] || req.connection.remoteAddress || req.ip;
+    let ipArrays = [];
+    if (!Array.isArray(ipaddr)) {
+        ipaddr = ipaddr.split(", ")
+        ipArrays = ipaddr;
+    } else {
+        ipArrays = ipaddr;
+    }
+    const IPCountry = realGetCountry(ipArrays);
+
     let filepath = req.path;
     if (filepath.startsWith("/")) {
         filepath = filepath.slice(1);
@@ -104,122 +120,83 @@ app.get("/:idpath", (req, res) => {
     if (filename_only.startsWith("ihacdn")) {
         filename_only = filename_only.slice(6);
     }
+    const realKey = filename_only;
     filename_only = "ihacdn" + filename_only;
-    console.log(`[CDNMapping] Trying to access: ${filename_only}`);
-    REDIS_INSTANCE.get(filename_only).then((get_data) => {
-        if (is_none(get_data)) {
-            console.error(`[CDNMapping] Key ${filename_only} doesn't exist`);
-            let missing_key = DELETED_ERROR;
-            missing_key = missing_key.replace(/\{\{ FN \}\}/g, filename_only);
-            res.status(404).end(missing_key);
-        } else {
-            if (get_data["type"] === "short") {
-                console.info(`[CDNMapping] Key ${filename_only} is a short link, redirecting...`);
-                res.redirect(get_data["target"]);
-            } else if (get_data["type"] === "code") {
-                console.info(`[CDNMapping] Key ${filename_only} are code type, rendering page...`);
-                readFile(get_data["path"], (err, buf) => {
-                    if (err) {
-                        REDIS_INSTANCE.delete(filename_only).then(() => {
-                            let gone_forever = DELETED_ERROR;
-                            gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
-                            res.status(410).end(gone_forever);
-                        }).catch(() => {
-                            let gone_forever = DELETED_ERROR;
-                            gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
-                            res.status(410).end(gone_forever);
-                        })
-                    } else {
-                        let code_contents = buf.toString();
-                        if (!is_none(code_contents) || code_contents) {
-                            let code_snippets = code_contents.slice(0, 10);
-                            res.render("codepaste", {
-                                FILENAME: filename_only,
-                                CODE_SNIPPETS: code_snippets,
-                                CODE_DATA: code_contents,
-                                CODE_TYPE: extension === "" ? get_data["mimetype"] : extension.slice(1),
-                            });
-                        } else {
-                            REDIS_INSTANCE.delete(filename_only).then(() => {
-                                let gone_forever = DELETED_ERROR;
-                                gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
-                                res.status(410).end(gone_forever);
-                            }).catch(() => {
-                                let gone_forever = DELETED_ERROR;
-                                gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
-                                res.status(410).end(gone_forever);
-                            })
-                        }
-                    }
-                })
-            } else if (get_data["type"] === "file") {
-                console.info(`[CDNMapping] Key ${filename_only} are file type, sending file...`);
-                try {
-                    if (!existsSync(get_data["path"])) {
-                        REDIS_INSTANCE.delete(filename_only).then(() => {
-                            let gone_forever = DELETED_ERROR;
-                            gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
-                            res.status(410).end(gone_forever);
-                        }).catch(() => {
-                            let gone_forever = DELETED_ERROR;
-                            gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
-                            res.status(410).end(gone_forever);
-                        })
-                    } else {
-                        res.sendFile(get_data["path"]);
-                    }
-                } catch (e) {
-                    console.log(get_data["path"]);
-                    console.error(e);
-                    REDIS_INSTANCE.delete(filename_only).then(() => {
-                        let gone_forever = DELETED_ERROR;
-                        gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
-                        res.status(410).end(gone_forever);
-                    }).catch(() => {
-                        let gone_forever = DELETED_ERROR;
-                        gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
-                        res.status(410).end(gone_forever);
-                    })
-                }
-            } else {
-                let missing_key = DELETED_ERROR;
-                missing_key = missing_key.replace(/\{\{ FN \}\}/g, filename_only);
-                res.status(404).end(missing_key);
-            }
-        }
-    }).catch(() => {
+    logger.info(`Trying to access: ${filename_only}`);
+    let redisData = await REDIS_INSTANCE.get(filename_only);
+    if (is_none(redisData)) {
+        logger.error(`Key ${filename_only} doesn't exist`);
         let missing_key = DELETED_ERROR;
         missing_key = missing_key.replace(/\{\{ FN \}\}/g, filename_only);
         res.status(404).end(missing_key);
-    })
+    } else {
+        if (redisData["type"] === "short") {
+            if (config.analytics.enable) {
+                await logAnalytics(realKey, IPCountry);
+            }
+            logger.info(`Key ${filename_only} is a short link, redirecting...`);
+            res.redirect(redisData["target"]);
+        } else if (redisData["type"] === "code") {
+            logger.info(`Key ${filename_only} are code type, rendering page...`);
+            readFile(redisData["path"], async (err, buf) => {
+                if (err) {
+                    await REDIS_INSTANCE.delete(filename_only);
+                    let gone_forever = DELETED_ERROR;
+                    gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
+                    res.status(410).end(gone_forever);
+                } else {
+                    let code_contents = buf.toString();
+                    if (is_none(code_contents) || !code_contents) {
+                        await REDIS_INSTANCE.delete(filename_only);
+                        let gone_forever = DELETED_ERROR;
+                        gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
+                        res.status(410).end(gone_forever);
+                    } else {
+                        let code_snippets = code_contents.slice(0, 10);
+                        res.render("codepaste", {
+                            FILENAME: filename_only,
+                            CODE_SNIPPETS: code_snippets,
+                            CODE_DATA: code_contents,
+                            CODE_TYPE: extension === "" ? redisData["mimetype"] : extension.slice(1),
+                        });
+                    }
+                }
+            })
+        } else if (redisData["type"] === "file") {
+            if (!existsSync(redisData["path"])) {
+                await REDIS_INSTANCE.delete(filename_only);
+                let gone_forever = DELETED_ERROR;
+                gone_forever = gone_forever.replace(/\{\{ FN \}\}/g, filename_only);
+                res.status(410).end(gone_forever);
+            } else {
+                res.sendFile(redisData["path"]);
+            }
+        } else {
+            let missing_key = DELETED_ERROR;
+            missing_key = missing_key.replace(/\{\{ FN \}\}/g, filename_only);
+            res.status(404).end(missing_key);
+        }
+    }
 })
 
-app.use(WinstonErrorLog({
-    transports: [
-        new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.ms(),
-        winston.format.printf((info) => {
-            return `[${info["timestamp"]}][${info.level}]: HTTP ${info["meta"]["req"]["httpVersion"]} ${info.message}`;
+app.use(expressErrorLogger);
+
+GeoIPInit().then(() => {
+    const listener = app.listen(6969, () => {
+        console.log("🚀 ihaCDN is now up and running!");
+        // @ts-ignore
+        console.log("http://127.0.0.1:" + listener.address().port);
+    });
+    
+    // Run retention clearance every hour
+    cron.schedule("*/60 * * * *", () => {
+        const logger = MainLogger.child({fn: "FileRetention"});
+        clearExpiredFile().then(() => {
+            // void
+        }).catch((err) => {
+            logger.error("[Retention] Error occured:");
+            console.error(err);
         })
-    ),
-}))
-
-const listener = app.listen(6969, () => {
-    console.log("🚀 ihaCDN is now up and running!");
-    // @ts-ignore
-    console.log("http://127.0.0.1:" + listener.address().port);
-})
-
-// Run retention clearance every hour
-cron.schedule("*/60 * * * *", () => {
-    clearExpiredFile().then(() => {
-        // void
-    }).catch((err) => {
-        console.error("[Retention] Error occured:");
-        console.error(err);
-    })
+    });
+    
 });
